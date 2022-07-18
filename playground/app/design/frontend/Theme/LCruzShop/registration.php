1bd441b4e50b63ac5049fcc18f7cfef981dc2405
luiquecruz <luiquecruz@gmail.com>
2022-07-18 01:54:16 -0300

    style: update theme registration reference

--- playground/app/design/frontend/Theme/LCruzShop/registration.php
+++ playground/app/design/frontend/Theme/LCruzShop/registration.php
@@ -2,4 +2,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Theme/lcruz-shop', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Theme/LCruzShop', __DIR__);
